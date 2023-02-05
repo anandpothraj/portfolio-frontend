519baa1cf5f265323e05f8e9ebdcc27b803e5a5d
anandpothraj <anandpothraj11052001@gmail.com>
2023-02-05 17:13:39 +0530

    Added Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import React from 'react';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
