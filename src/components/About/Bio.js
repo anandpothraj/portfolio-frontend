6b1737779bbb0c23ad389b6b18e587192ade81c0
anandpothraj <anandpothraj11052001@gmail.com>
2023-02-08 23:57:54 +0530

    Creating Bio section

---
+++ src/components/About/Bio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bio = () => {
+  return (
+    <div>
+        <p className="bioText">Hi there! Thanks for visiting my portfolio. <br/>
+        I'm <span>Anand Pothraj</span>, a MERN Stack Developer, Content Writer, and a passionate learner. I had completed Bachelor's In Computer Science from Mumbai University in 2022. I have worked on a wide range of technologies and have worked on projects ranging from small to large scale. I am a self-motivated and self-driven individual who is always looking for new challenges and opportunities. I love to get engage in communities and learn from others.</p>
+    </div>
+  )
+}
+
+export default Bio
