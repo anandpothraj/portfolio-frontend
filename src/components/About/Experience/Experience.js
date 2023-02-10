7fc02e5a9d7d24421db63b66aa18c8109c5003de
anandpothraj <anandpothraj11052001@gmail.com>
2023-02-11 00:00:02 +0530

    Added Experience Section

--- src/components/About/Experience/Experience.js
+++ src/components/About/Experience/Experience.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import ExperienceItem from './ExperienceItem';
+import { ExperienceDetails } from './ExperienceDetails';
 
 const Experience = () => {
-  return (
-    <div id="experience">
-      <Container>
-        <div className="experience">
-          <h2>Experience</h2>
-          <div className="experience__content">
 
-            <ExperienceItem
-              company="Google Developer Student Club - LPU"
-              companyImgUrl="https://pbs.twimg.com/profile_images/1425284890799460354/phO6uvw5_400x400.jpg"
-              type="Volunteering"
-              position="Team Member"
-              startDate="Mar 2022"
-              endDate="Present"
-            />
-            <div className="line"></div>
-          </div>
-        </div>
-      </Container>
+  return (
+    <div className='my-2 my-md-5'>
+      <h2 className='colorOffWhite my-4 textCenter'>Experience</h2>
+      <div className="my-2 my-md-5">
+        {ExperienceDetails.map((data, index) => 
+        <div key={index}>
+          <ExperienceItem props={data} />
+          <div className="lineMark"></div>
+        </div>)}
+      </div>
     </div>
   );
 };
---
+++ src/components/About/Experience/ExperienceDetails.js
@@ -0,0 +1,26 @@
+export const ExperienceDetails = [
+    {
+        company : "Google Developer Student Club - LPU",
+        companyImgUrl : "https://pbs.twimg.com/profile_images/1425284890799460354/phO6uvw5_400x400.jpg",
+        type : "Volunteering", 
+        position : "Team Member",
+        startDate : "Mar 2022",
+        endDate : "Present",
+    },
+    {
+        company : "Google Developer Student Club - LPU",
+        companyImgUrl : "https://pbs.twimg.com/profile_images/1425284890799460354/phO6uvw5_400x400.jpg",
+        type : "Volunteering", 
+        position : "Team Member",
+        startDate : "Mar 2022",
+        endDate : "Present",
+    },
+    {
+        company : "Google Developer Student Club - LPU",
+        companyImgUrl : "https://pbs.twimg.com/profile_images/1425284890799460354/phO6uvw5_400x400.jpg",
+        type : "Volunteering", 
+        position : "Team Member",
+        startDate : "Mar 2022",
+        endDate : "Present",
+    },
+];
