13cd7ce8dff1177dd37312888e3b644abca057d3
anandpothraj <anandpothraj11052001@gmail.com>
2023-02-12 16:21:07 +0530

    Created Support Section

---
+++ src/components/About/Support.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Support = () => {
+  return (
+    <div className='my-md-3 my-5'>
+        <h3>Support</h3>
+    </div>
+  )
+}
+
+export default Support
--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,7 @@ import AboutInfo from '../components/About/AboutInfo';
 import Experience from '../components/About/Experience/Experience';
 import Technology from '../components/About/Technology/Technology';
 import ContributionsGraph from '../components/About/ContributionGraph';
+import Support from '../components/About/Support';
 
 const About = () => {
   return (
@@ -19,6 +20,7 @@ const About = () => {
       <Experience/>
       <Technology/>
       <Services/>
+      <Support/>
     </div>
   );
 };
